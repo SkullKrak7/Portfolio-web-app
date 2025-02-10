bed2cf462d0777d36a0fb8ff59a995d70616ad14
[Karthik Kagolanu] <[sai.kagolanu@yahoo.com]>
2025-02-10 20:36:48 +0000

    Used environment variable

--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "testerburner0@gmail.com"
-    password = "qunczlfbtnvhitiy"
+    password = os.getenv(PASSWORD)
 
     receiver = "testerburner0@gmail.com"
     my_context = ssl.create_default_context()
